c86def932901d2c9094ccadb801a56fa68a38a5e
Harsheen Kaur Kohli <122385400+harsheenkohli@users.noreply.github.com>
2023-08-16 22:54:10 +0530

    Create OddEvenSumOptimised.java

---
+++ OddEvenSumOptimised.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class OddEvenSumOptimised {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.next();
+		char[] a = str.toCharArray();
+		int[] arr = new int[a.length];
+		for(int i=0;i<a.length;i++){
+			arr[i] = Character.getNumericValue(a[i]);
+		}
+		int sum1=0,sum2=0;
+		for(int i : arr){
+			if(i%2==0)
+				sum1+=i;
+			else
+				sum2+=i;
+		}
+		
+		System.out.print(sum1 + " "+ sum2);
+
+	}
+}
